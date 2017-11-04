c8d74c0923fe78ef26d41b842906581af0544fac
Van Pho Nguyen <vanpho02@gmail.com>
2017-11-04 18:32:00 +0700

    func in func

--- a.js
+++ a.js
@@ -23,7 +23,8 @@ function inHinh2(n) {
 function inHinh3(n) {
     for(let i = 1; i <= n; i++) {
         let s = '';
-        for(let j = 1; j <= n; j++) {
+        const soCot = n;
+        for(let j = 1; j <= soCot; j++) {
             const dk = i <= j;
             s += dk ? '*' : ' ';
         }
@@ -31,10 +32,23 @@ function inHinh3(n) {
     }
 }
 
-function inHinh(n, checkPoint) {
+function inHinh4(n) {
     for(let i = 1; i <= n; i++) {
         let s = '';
-        for(let j = 1; j <= n; j++) {
+        const soCot = 2 * n - 1;
+        for(let j = 1; j <= soCot; j++) {
+            const dk = Math.abs(j - n) <= i - 1;
+            s += dk ? '*' : ' ';
+        }
+        console.log(s);
+    }
+}
+
+function inHinh(n, checkPoint, getSoCot) {
+    for(let i = 1; i <= n; i++) {
+        let s = '';
+        const soCot = getSoCot ? getSoCot(n) : n;
+        for(let j = 1; j <= soCot; j++) {
             const dk = checkPoint(i, j, n);
             s += dk ? '*' : ' ';
         }
@@ -45,18 +59,8 @@ function inHinh(n, checkPoint) {
 inHinh(5, (i, j) => i >= j);
 inHinh(5, (i, j, n) => i + j >= n + 1);
 inHinh(5, (i, j) => i <= j);
+inHinh(5, (i, j, n) => Math.abs(j - n) <= i - 1, n => 2 * n - 1);
 
-function inHinh4(n) {
-    for(let i = 1; i <= n; i++) {
-        let s = '';
-        const soCot = 2 * n - 1;
-        for(let j = 1; j <= soCot; j++) {
-            const khoangCach = Math.abs(j - n);
-            const dk = khoangCach <= i - 1;
-            s += dk ? '*' : ' ';
-        }
-        console.log(s);
-    }
-}
+
 
 // inHinh4(5);
---
+++ b.js
@@ -0,0 +1,5 @@
+function add(a, b) {
+    console.log(a + b);
+} 
+
+console.log(add(4));
